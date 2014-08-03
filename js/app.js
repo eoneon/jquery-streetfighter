04a50bea29117284011ef9912de2234099e3465b
eon <eoneon123@gmail.com>
2014-08-03 09:25:27 -0700

    part 1

---
+++ js/app.js
@@ -0,0 +1,15 @@
+/*function enter(){
+	$("#standing").hide().$("#ready").show();
+}
+function leave(){
+	$("#standing").show().$("#ready, #throwing").hide();
+}
+function throwing(){
+	$("#ready").hide().$("#throwing").show();
+}
+$(document).ready(function(){
+	$(".main").on("mouseenter", enter);
+	$(".main").on("mouseleave", leave);
+	$(".main").on("click", throwing);
+
+});*/
